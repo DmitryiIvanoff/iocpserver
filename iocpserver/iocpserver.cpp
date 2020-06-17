fb48a39897ffc27be35065466367651c148e91a6
git <DmtrIvanoff@yandex.ru>
2020-06-17 15:32:01 +0300

    Небольшие изменения с const

--- iocpserver/iocpserver.cpp
+++ iocpserver/iocpserver.cpp
@@ -3,8 +3,8 @@
 #include "IOCPProxyLoggingServer.h"
 
 bool g_bEndServer;
-static std::string g_sClientPort = "4000";
-static std::string g_sMySQLPort = "3306";
+const std::string g_sClientPort = "4000";
+const std::string g_sMySQLPort = "3306";
 const int g_dDBThreadsCount = 16;
 const int g_dClientThreadsCount = 10;
 
